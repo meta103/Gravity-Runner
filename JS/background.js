d11edcc3dbea67aff4b441bd75d72d611f34b30d
Marcos Tagliabue <marcostagliabue103@gmail.com>
2019-01-19 11:34:27 +0100

    MVP done

--- JS/background.js
+++ JS/background.js
@@ -2,7 +2,7 @@ class Background {
 
   constructor (){
     this.img = new Image ();
-    this.img.src = "FondoCanvas3.png";
+    this.img.src = "fondocanvas3.png";
     this.x = 0;
     this.width = 1019;
     this.speed = -1;
